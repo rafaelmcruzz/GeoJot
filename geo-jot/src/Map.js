37b9e77ce68ed62fe9466d59a3f245ed334c6ec8
Hughee04 <74217936+Hughee04@users.noreply.github.com>
2024-03-06 15:22:53 +0000

    Added Map

---
+++ geo-jot/src/Map.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+function Map() {
+  const position = [51.505, -0.09];
+
+  return (
+    <div className="map">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '1000px' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
