bf51e4b904f032e53ee5744a564232c1072f8e6b
Hughee04 <74217936+Hughee04@users.noreply.github.com>
2024-03-06 16:05:30 +0000

    Changed Map Size

--- geo-jot/src/Map.js
+++ geo-jot/src/Map.js
@@ -7,7 +7,7 @@ function Map() {
 
   return (
     <div className="map">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '1000px' }}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '955px' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
