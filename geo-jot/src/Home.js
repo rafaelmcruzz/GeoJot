56d5a2d899feb48ba70ad72864ec1c8d956d7d0b
Hughee04 <74217936+Hughee04@users.noreply.github.com>
2024-03-06 15:34:10 +0000

    Update Home

--- geo-jot/src/Home.js
+++ geo-jot/src/Home.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import './Home.css'; // 引入 CSS 样式文件
+import Map from './Map';
 
-function Home() {
+
+// LeftSidebar 组件
+function LeftSidebar({ user, addresses, selectedAddress }) {
+  return (
+    <div className="left-sidebar">
+      <div className="user-profile">
+        <img src={user.avatar} alt="User Avatar" />
+        <p>{user.name}</p>
+      </div>
+      <div className="address-block">
+        <h2>My Addresses</h2>
+        <ul>
+          {addresses.map((address, index) => (
+            <li key={index}>
+              <p>{address.name}</p>
+              {/* 可以根据需要显示其他地址信息 */}
+              <p>{address.notes}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="selected-address-details">
+        <h2>Selected Address Details</h2>
+        <p>Address Name: {selectedAddress.name}</p>
+        <p>Address Notes: {selectedAddress.notes}</p>
+      </div>
+    </div>
+  );
+}
+
+// 主界面组件
+function MainContent() {
+  return (
+    <div className="main-content">
+      <Map />
+    </div>
+  );
+}
+
+// App 组件
+function App() {
   // 模拟用户和地址数据
   const user = {
     name: 'John Doe',
@@ -19,55 +60,15 @@ function Home() {
     notes: 'Notes for Selected Address',
   };
 
-  // LeftSidebar 组件
-  function LeftSidebar() {
-    return (
-      <div className="left-sidebar">
-        <div className="user-profile">
-          <img src={user.avatar} alt="User Avatar" />
-          <p>{user.name}</p>
-        </div>
-        <div className="address-block">
-          <h2>My Addresses</h2>
-          <ul>
-            {addresses.map((address, index) => (
-              <li key={index}>
-                <p>{address.name}</p>
-                {/* 可以根据需要显示其他地址信息 */}
-                <p>{address.notes}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="selected-address-details">
-          <h2>Selected Address Details</h2>
-          <p>Address Name: {selectedAddress.name}</p>
-          <p>Address Notes: {selectedAddress.notes}</p>
-        </div>
-      </div>
-    );
-  }
-
-  // 主界面组件
-  function MainContent() {
-    return (
-      <div className="main-content">
-        {/* 这里添加地图组件 */}
-        <div className="map">
-          {/* 添加地图内容 */}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="app">
       {/* 左侧栏 */}
-      <LeftSidebar />
+      <LeftSidebar user={user} addresses={addresses} selectedAddress={selectedAddress} />
       {/* 主内容区 */}
       <MainContent />
     </div>
   );
 }
 
-export default Home;
+export default App;
+
