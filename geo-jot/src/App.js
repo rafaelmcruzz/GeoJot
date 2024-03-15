b5c5db727d094bb7d88dc42c8df311e45bd2a76e
aleptian <nahasseif@gmail.com>
2024-03-15 13:14:09 +0000

    uncommented css

--- geo-jot/src/App.js
+++ geo-jot/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import './styles.css'; // Import your global CSS file
+import './styles.css'; // Import your global CSS file
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Login'; // Import the Login component
 //import Register from './Register';
